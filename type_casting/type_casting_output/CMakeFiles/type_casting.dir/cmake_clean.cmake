fc0bf57c03025b6921b0f852017b94527f44d962
neovrnaz <neovrnaz@gmail.com>
2020-11-21 01:47:49 -0700

    Add type_casting directory

---
+++ type_casting/type_casting_output/CMakeFiles/type_casting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/type_casting.dir/main.c.o"
+  "type_casting"
+  "type_casting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/type_casting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
