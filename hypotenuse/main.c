5e1364500637d91fc80366d4bd2735f58523a6e4
neovrnaz <neovrnaz@gmail.com>
2020-11-22 14:17:11 -0700

    Remove TODO

--- hypotenuse/main.c
+++ hypotenuse/main.c
@@ -1,24 +1,31 @@
+/*
+ * Takes two ints as inputs and outputs
+ * the hypotenuse of a right triangle
+ *
+ * Date: Nov 22, 2020
+ * Author: Elijah Gray
+ * */
+
 #include <stdio.h>
 #include <math.h>
 
 int main() {
-  // TODO: Replace int with double
-  double adjacentSide;
-  double oppositeSide;
+  int adjacentSide;
+  int oppositeSide;
 
   printf("This app will calculate the hypotenuse of a triangle!\n\n");
 
   printf("Enter the adjacent side: ");
-  scanf("%lf", &adjacentSide);
+  scanf("%i", &adjacentSide);
 
   printf("Enter the opposite side: ");
-  scanf("%lf", &oppositeSide);
+  scanf("%i", &oppositeSide);
 
-  double cSquared = adjacentSide * adjacentSide + oppositeSide * oppositeSide;
-  double hypotenuse = sqrt(cSquared);
+  int cSquared = adjacentSide * adjacentSide + oppositeSide * oppositeSide;
+  int hypotenuse = sqrt(cSquared);
 
-  printf("\n%lf² + %lf² = %lf²\n", adjacentSide, oppositeSide, cSquared);
-  printf("Hypotenuse = %lf\n", hypotenuse);
+  printf("\n%i² + %i² = %i²\n", adjacentSide, oppositeSide, cSquared);
+  printf("Hypotenuse = %i\n", hypotenuse);
 
   return 0;
 }
