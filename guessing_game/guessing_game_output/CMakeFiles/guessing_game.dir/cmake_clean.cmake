e1feb3a2446b31cb336bfb7c68ef50097b8c5e65
neovrnaz <neovrnaz@gmail.com>
2020-11-23 03:14:51 -0700

    Add guessing game to CMakeLists

---
+++ guessing_game/guessing_game_output/CMakeFiles/guessing_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guessing_game.dir/main.c.o"
+  "guessing_game"
+  "guessing_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/guessing_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
