23f6ef2b416a0248e3e7d5842524994f0b1e722f
neovrnaz <neovrnaz@gmail.com>
2020-11-27 03:53:54 -0700

    Add structs subproject

---
+++ structs/structs/CMakeFiles/structs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/structs.dir/main.c.o"
+  "structs"
+  "structs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/structs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
